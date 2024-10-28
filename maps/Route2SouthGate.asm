22645754ba414c8f46d556e4cca3138fe9fcf6c9
ShinySylveon01 <ShinySylveon01@github.com>
2024-10-28 18:01:59 -0400

    Added an NPC in the Viridian Forest South Gate and fixed the issue with the trainer script event flags

--- maps/Route2SouthGate.asm
+++ maps/Route2SouthGate.asm
@@ -1,9 +1,21 @@
 	object_const_def ; object_event constants
+	const ROUTE2SOUTHGATE_OFFICER
 
 Route2SouthGate_MapScripts:
 	db 0 ; scene scripts
 
 	db 0 ; callbacks
+
+Route2SouthGateOfficerScript:
+	jumptextfaceplayer Route2SouthGateOfficerText
+
+Route2SouthGateOfficerText:
+	text "VIRIDIAN FOREST is"
+	line "like a maze."
+
+	para "Some BUG #MON"
+	line "can poison you!"
+	done
 
 Route2SouthGate_MapEvents:
 	db 0, 0 ; filler
@@ -19,4 +31,5 @@ Route2SouthGate_MapEvents:
 
 	db 0 ; bg events
 
-	db 0 ; object events
+	db 1 ; object events
+	object_event  0,  3, SPRITE_OFFICER, SPRITEMOVEDATA_STANDING_RIGHT, 0, 0, -1, -1, PAL_NPC_RED, OBJECTTYPE_SCRIPT, 0, Route2SouthGateOfficerScript, -1
