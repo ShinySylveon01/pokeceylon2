d436a9413187add13a4099afa15718d8940ecde7
ShinySylveon01 <shakibafzali@gmail.com>
2025-10-20 01:18:16 -0400

    Finished editing tree Pokemon data

--- data/wild/treemons.asm
+++ data/wild/treemons.asm
@@ -15,78 +15,78 @@ TreeMonSet_None:
 TreeMonSet_Unused:
 TreeMonSet_City:
 ; common
-	db 50, VENONAT,    15
-	db 30, VENONAT,    15
+	db 50, SHUCKLE,    15
+	db 30, LEDYBA,     15
 	db 10, ABRA,       15
-	db  5, ABRA,       15
-	db  5, VENOMOTH,   15
+	db  5, KADABRA,    16
+	db  5, LEDIAN,     15
 	db -1
 ; rare
-	db 50, VENONAT,    15
-	db 30, MAGNEMITE,  15
+	db 50, SHUCKLE,    15
+	db 30, LEDYBA,     15
 	db 10, ABRA,       15
-	db  5, ABRA,       15
-	db  5, VENOMOTH,   15
+	db  5, KADABRA,    16
+	db  5, LEDIAN,     15
 	db -1
 
 TreeMonSet_Forest:
 
 IF DEF(_GOLD)
 ; common
-	db 50, CATERPIE,   10
-	db 15, CATERPIE,   10
-	db 15, METAPOD,    10
+	db 50, PINSIR,     10
+	db 15, WEEDLE,     10
+	db 15, YANMA,      10
 	db 10, EXEGGCUTE,  10
-	db  5, EXEGGCUTE,  10
-	db  5, BUTTERFREE, 10
+	db  5, BEEDRILL,   10
+	db  5, SHUCKLE,    10
 	db -1
 ; rare
-	db 50, CATERPIE,   10
-	db 15, PINECO,     10
-	db 15, PINECO,     10
+	db 50, PINSIR,     10
+	db 15, WEEDLE,     10
+	db 15, YANMA,      10
 	db 10, EXEGGCUTE,  10
-	db  5, EXEGGCUTE,  10
-	db  5, BUTTERFREE, 10
+	db  5, BEEDRILL,   10
+	db  5, SHUCKLE,    10
 	db -1
 
 ELIF DEF(_SILVER)
 ; common
-	db 50, WEEDLE,     10
+	db 50, PINSIR,     10
 	db 15, WEEDLE,     10
-	db 15, KAKUNA,     10
+	db 15, YANMA,      10
 	db 10, EXEGGCUTE,  10
-	db  5, EXEGGCUTE,  10
 	db  5, BEEDRILL,   10
+	db  5, SHUCKLE,    10
 	db -1
 ; rare
-	db 50, WEEDLE,     10
-	db 15, PINECO,     10
-	db 15, PINECO,     10
+	db 50, PINSIR,     10
+	db 15, WEEDLE,     10
+	db 15, YANMA,      10
 	db 10, EXEGGCUTE,  10
-	db  5, EXEGGCUTE,  10
 	db  5, BEEDRILL,   10
+	db  5, SHUCKLE,    10
 	db -1
 ENDC
 
 TreeMonSet_Canyon:
 ; common
-	db 50, SPEAROW,    10
-	db 15, SPEAROW,    10
-	db 15, SPEAROW,    10
-	db 10, AIPOM,      10
-	db  5, AIPOM,      10
-	db  5, AIPOM,      10
+	db 50, NATU,       10
+	db 15, PIDGEY,     10
+	db 15, TOGEPI,     10
+	db 10, SPEAROW,    10
+	db  5, PINECO,     10
+	db  5, SPINARAK,   10
 	db -1
 ; rare
-	db 50, SPEAROW,    10
-	db 15, HERACROSS,  10
-	db 15, HERACROSS,  10
-	db 10, AIPOM,      10
-	db  5, AIPOM,      10
-	db  5, AIPOM,      10
+	db 50, NATU,       10
+	db 15, PIDGEY,     10
+	db 15, TOGEPI,     10
+	db 10, SPEAROW,    10
+	db  5, PINECO,     10
+	db  5, SPINARAK,   10
 	db -1
 
 TreeMonSet_Rock:
-	db 90, KRABBY,     15
-	db 10, SHUCKLE,    15
+	db 90, SCYTHER,    15
+	db 10, HERACROSS,  15
 	db -1
