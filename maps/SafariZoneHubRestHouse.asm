7a5c916cd3587644232c3a2d39de3572ece2b4f5
ShinySylveon01 <shakibafzali@gmail.com>
2026-08-15 23:59:38 -0400

    Added an NPC in the Safari Zone Hub Rest House

--- maps/SafariZoneHubRestHouse.asm
+++ maps/SafariZoneHubRestHouse.asm
@@ -1,7 +1,23 @@
+	object_const_def ; object_event constants
+	const SAFARI_ZONE_HUB_REST_HOUSE_YOUNGSTER
+
 SafariZoneHubRestHouse_MapScripts:
 	db 0 ; scene scripts
 
 	db 0 ; callbacks
+	
+SafariZoneHubRestHouseYoungsterScript:
+	jumptextfaceplayer SafariZoneHubRestHouseYoungsterText
+
+SafariZoneHubRestHouseYoungsterText:
+	text "I took a break"
+	line "from catching"
+	cont "#MON!"
+
+	para "It's nice to relax"
+	line "and take it easy"
+	cont "on yourself!"
+	done
 
 SafariZoneHubRestHouse_MapEvents:
 	db 0, 0 ; filler
@@ -14,4 +30,5 @@ SafariZoneHubRestHouse_MapEvents:
 
 	db 0 ; bg events
 
-	db 0 ; object events
+	db 1 ; object events
+	object_event  3,  3, SPRITE_YOUNGSTER, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, PAL_NPC_RED, OBJECTTYPE_SCRIPT, 0, SafariZoneHubRestHouseYoungsterScript, -1
